c8856d36d455937852882739752ea6a943e42d20
SethSR <srenshaw1@gmail.com>
2015-09-23 00:50:16 -0500

    added variables from game doc

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,7 +1,14 @@
-public enum Movement {
-	Run,
-	Tread,
-	Wheel,
-	Hover,
-	Booster,
+// public enum Movement {
+// 	Run,
+// 	Tread,
+// 	Wheel,
+// 	Hover,
+// 	Booster,
+// }
+
+public class Movement : MonoBehaviour {
+	public bool aerial;    // whether the robot becomes airborne
+	public float cooldown; // time until re-activation
+	public float distance; // how far the robot will move
+	public float speed;    // how fast the robot will move
 }
