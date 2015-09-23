f0b54e4a27f538fcd98cd540cd475553a5908723
SethSR <srenshaw1@gmail.com>
2015-09-23 00:51:42 -0500

    added stuff from the game doc

--- Assets/Scripts/Offense.cs
+++ Assets/Scripts/Offense.cs
@@ -1,11 +1,41 @@
-public enum Offense {
-	Pistol,
-	EPistol,
-	Rifle,
-	ERifle,
-	Sniper,
-	ESniper,
-	Gatling,
-	Rocket,
-	Grenades,
+// public enum Offense {
+// 	Pistol,
+// 	EPistol,
+// 	Rifle,
+// 	ERifle,
+// 	Sniper,
+// 	ESniper,
+// 	Gatling,
+// 	Rocket,
+// 	Grenades,
+// }
+
+public class Offense : MonoBehaviour {
+	public Transform bullet;
+
+	public float cooldown; // time until re-activation
+	public float distance; // how far the attack can travel
+	public float impact;   // size of the area affected by the attack
+	public float power;    // attack damage and stun length
+	public float speed;    // attack movement speed
+	public int   hits;     // number of attacks launched
+
+	public bool isCooled {
+		get { return timer < 0; }
+	}
+
+	public void activate() {
+		if (isCooled) {
+			timer = cooldown;
+			Rigidbody2D bullet = (Rigidbody2D)Instantiate(bullet);
+		}
+	}
+
+	float timer;
+
+	void Update() {
+		if (!isCooled) {
+			timer -= Time.deltaTime;
+		}
+	}
 }
