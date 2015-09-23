9550f621bfaf9a45a887861eac898c2f69ea11b3
SethSR <srenshaw1@gmail.com>
2015-09-23 00:30:03 -0500

    formatting

--- Assets/Scripts/Behaviors/Seek.cs
+++ Assets/Scripts/Behaviors/Seek.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 
 public class Seek {
-	static public Vector2 calculate(Vector2 pos, Vector2 vel, Vector2 target, float max_speed) {
+	static public Vector2 calculate(Vector2 pos,
+	                                Vector2 vel,
+	                                Vector2 target,
+	                                float   max_speed) {
 		var desired_velocity = (target - pos).normalized * max_speed;
 		return (desired_velocity - vel);
 	}
 }
 
 public class Arrive {
-	static public Vector2 calculate(Vector2 pos, Vector2 vel, Vector2 target, float max_speed, float decel) {
+	static public Vector2 calculate(Vector2 pos,
+	                                Vector2 vel,
+	                                Vector2 target,
+	                                float   max_speed,
+	                                float   decel) {
 		var to_target = target - pos;
 		var dist = to_target.magnitude;
 
