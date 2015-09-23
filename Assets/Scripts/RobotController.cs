30b0fe6e6d454a6763bed5e2ea891246aac08a09
SethSR <srenshaw1@gmail.com>
2015-09-22 21:50:36 -0500

    moved maxSpeed here from PlayerController

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 [RequireComponent(typeof(Robot))]
 [RequireComponent(typeof(Rigidbody2D))]
 abstract public class RobotController : MonoBehaviour {
+	public float maxSpeed = 10; // 10 m/s
+	
 	// Use this for initialization
 	void Start () {
 	}
