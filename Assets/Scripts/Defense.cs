9779c85362643cf8e6c0f6f0da376457da083376
SethSR <srenshaw1@gmail.com>
2015-09-23 00:51:54 -0500

    added stuff from the game doc

--- Assets/Scripts/Defense.cs
+++ Assets/Scripts/Defense.cs
@@ -1,6 +1,13 @@
-public enum Defense {
-	Shield,
-	EShield,
-	Dodge,
-	Parry,
+// public enum Defense {
+// 	Shield,
+// 	EShield,
+// 	Dodge,
+// 	Parry,
+// }
+
+public class Defense : MonoBehaviour {
+	public float cooldown; // time until re-activation
+	public float power;    // damage reduction
+	public float time;     // how long the shield lasts
+	public float parry;    // the percent of damage deflected [over 100% reflects the shot back]
 }
