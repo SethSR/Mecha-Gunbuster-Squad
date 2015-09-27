e622cd13f31b431535f1312f3272fef06e57c7ed
SethSR <srenshaw1@gmail.com>
2015-09-27 03:21:02 -0500

    Added temporary fuzzy logic rules

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -24,7 +24,7 @@ public class AIController : RobotController {
 		need         = fm.CreateFLV("Need");
 
 		targetClose  = distToTarget.AddLeftShoulderSet ("Target_Close" ,   0,  25, 150);
-		targetMedium = distToTarget.AddTriangularSet   ("Target_Medium",  25,  50, 300);
+		targetMedium = distToTarget.AddTriangularSet   ("Target_Medium",  25,  50, 150);
 		targetFar    = distToTarget.AddRightShoulderSet("Target_Far"   , 150, 300, 500);
 
 		ammoLow  = ammoCount.AddLeftShoulderSet ("Ammo_Low" ,  0, 25,  50);
@@ -34,8 +34,17 @@ public class AIController : RobotController {
 		undesirable = need.AddLeftShoulderSet ("Undesirable",  0, 33,  67);
 		desirable   = need.AddRightShoulderSet(  "Desirable", 33, 67, 100);
 
-		fm.AddRule(FuzzyHelpers.and(targetFar,ammoLow), undesirable);
-		fm.AddRule(targetFar.and(ammoLow), desirable);
+		fm.AddRule(targetClose.and(ammoLow) , undesirable);
+		fm.AddRule(targetClose.and(ammoMid) ,   desirable);
+		fm.AddRule(targetClose.and(ammoHigh),   desirable);
+
+		fm.AddRule(targetMedium.and(ammoLow) , undesirable);
+		fm.AddRule(targetMedium.and(ammoMid) , undesirable);
+		fm.AddRule(targetMedium.and(ammoHigh),   desirable);
+
+		fm.AddRule(targetFar.and(ammoLow) , undesirable);
+		fm.AddRule(targetFar.and(ammoMid) , undesirable);
+		fm.AddRule(targetFar.and(ammoHigh), undesirable);
 	}
 	
 	// Update is called once per frame
