548ec4980649a404d2e0e8548f01f5515c824b8f
SethSR <srenshaw1@gmail.com>
2015-09-27 18:38:12 -0500

    format changes

--- Assets/Scripts/FuzzyLogic/FuzzySet.cs
+++ Assets/Scripts/FuzzyLogic/FuzzySet.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using FuzzyLogic;
+
 namespace FuzzyLogic {
 	abstract public class FuzzySet {
 		// this will hold the degree of membership in this set of a given value
@@ -14,7 +16,7 @@ namespace FuzzyLogic {
 		float m_dRepresentativeValue;
 
 		public FuzzySet(float RepVal) {
-			m_dDOM = 0.0f;
+			m_dDOM = 0;
 			m_dRepresentativeValue = RepVal;
 		}
 
@@ -35,27 +37,10 @@ namespace FuzzyLogic {
 		}
 
 		// accessor methods
-		public float GetRepresentativeVal()           { return m_dRepresentativeValue; }
-		public void  ClearDOM            ()           { m_dDOM = 0.0f; }
-		public float GetDOM              ()           { return m_dDOM; }
+		public float GetRepresentativeVal()          { return m_dRepresentativeValue; }
+		public void  ClearDOM            ()          { m_dDOM = 0; }
+		public float GetDOM              ()          { return m_dDOM; }
 		public void  SetDOM              (float val) { m_dDOM = val; }
-
-		static public bool isEqual(float a, float b) { return isEqual(a,b,0.0001f); }
-		static public bool isEqual(float a, float b, float epsilon) {
-			float absA = Mathf.Abs(a);
-			float absB = Mathf.Abs(b);
-			float diff = Mathf.Abs(a - b);
-
-			if (a == b) { // shortcut, handles infinities
-				return true;
-			} else if (a == 0 || b == 0 || diff < float.MinValue) {
-				// a or b is zero or both are extremely close to it
-				// relative error is less meaningful here
-				return diff < (epsilon * float.MinValue);
-			} else { // use relative error
-				return diff / (absA + absB) < epsilon;
-			}
-		}
 	}
 
 	public class FuzzySet_LeftShoulder : FuzzySet {
@@ -65,17 +50,21 @@ namespace FuzzyLogic {
 			m_dRightOffset = rgt;
 		}
 
+		// A := m_dRightOffset == 0
+		// B := m_dLeftOffset == 0
+		// C := m_dPeakPoint == val
+		// (A*C)+(B*C) == (A+B)*C
+
 		override public float CalculateDOM(float val) {
-			if ((isEqual(m_dRightOffset, 0.0f) && (isEqual(m_dPeakPoint, val))) ||
-			    (isEqual(m_dLeftOffset, 0.0f) && (isEqual(m_dPeakPoint, val)))) {
-				return 1.0f;
+			if ((m_dRightOffset.isEqual(0) || m_dLeftOffset.isEqual(0)) && m_dPeakPoint.isEqual(val)) {
+				return 1;
 			} else if ((val >= m_dPeakPoint) && (val < (m_dPeakPoint + m_dRightOffset))) {
-				float grad = 1.0f / -m_dRightOffset;
-				return grad * (val - m_dPeakPoint) + 1.0f;
+				float grad = 1 / -m_dRightOffset;
+				return grad * (val - m_dPeakPoint) + 1;
 			} else if ((val < m_dPeakPoint) && (val >= m_dPeakPoint - m_dLeftOffset)) {
-				return 1.0f;
+				return 1;
 			} else {
-				return 0.0f;
+				return 0;
 			}
 		}
 
@@ -85,9 +74,7 @@ namespace FuzzyLogic {
 	}
 
 	public class FuzzySet_RightShoulder : FuzzySet {
-		public FuzzySet_RightShoulder(float peak,
-		                              float lft,
-		                              float rgt) : base(((peak + rgt) + peak) / 2) {
+		public FuzzySet_RightShoulder(float peak, float lft, float rgt) : base(((peak + rgt) + peak) / 2) {
 			m_dPeakPoint   = peak;
 			m_dLeftOffset  = lft;
 			m_dRightOffset = rgt;
@@ -95,16 +82,15 @@ namespace FuzzyLogic {
 
 		// this method calculates the degree of membership for a particular value
 		override public float CalculateDOM(float val) {
-			if ((isEqual(m_dRightOffset, 0.0f) && (isEqual(m_dPeakPoint, val))) ||
-			    (isEqual(m_dLeftOffset, 0.0f) && (isEqual(m_dPeakPoint, val)))) {
-				return 1.0f;
+			if ((m_dRightOffset.isEqual(0) || m_dLeftOffset.isEqual(0)) && m_dPeakPoint.isEqual(val)) {
+				return 1;
 			} else if ((val <= m_dPeakPoint) && (val > (m_dPeakPoint - m_dLeftOffset))) {
-				float grad = 1.0f / m_dLeftOffset;
+				float grad = 1 / m_dLeftOffset;
 				return grad * (val - (m_dPeakPoint - m_dLeftOffset));
 			} else if ((val > m_dPeakPoint) && (val <= m_dPeakPoint + m_dRightOffset)) {
-				return 1.0f;
+				return 1;
 			} else {
-				return 0.0f;
+				return 0;
 			}
 		}
 
@@ -115,64 +101,46 @@ namespace FuzzyLogic {
 	}
 
 	public class FuzzySet_Triangle : FuzzySet {
-		public FuzzySet_Triangle(float mid,
-		                         float lft,
-		                         float rgt) : base(mid) {
+		public FuzzySet_Triangle(float mid, float lft, float rgt) : base(mid) {
 			m_dPeakPoint   = mid;
 			m_dLeftOffset  = lft;
 			m_dRightOffset = rgt;
 		}
 
-		// this method calculates the degree of membership for a particular value
 		override public float CalculateDOM(float val) {
-			// test for the case where the triangle's left or right offsets are zero
-			// (to prevent divide by zero errors below)
-			if ((isEqual(m_dRightOffset, 0.0f) && (isEqual(m_dPeakPoint, val))) ||
-			    (isEqual(m_dLeftOffset, 0.0f) && (isEqual(m_dPeakPoint, val)))) {
-				return 1.0f;
-			}
-
-			// find DOM if left of center
-			if ((val <= m_dPeakPoint) && (val >= (m_dPeakPoint - m_dLeftOffset))) {
-				float grad = 1.0f / m_dLeftOffset;
+			if ((m_dRightOffset.isEqual(0) || m_dLeftOffset.isEqual(0)) && m_dPeakPoint.isEqual(val)) {
+				return 1;
+			} else if ((val <= m_dPeakPoint) && (val >= (m_dPeakPoint - m_dLeftOffset))) {
+				float grad = 1 / m_dLeftOffset;
 				return grad * (val - (m_dPeakPoint - m_dLeftOffset));
-			}
-			// find DOM if right of center
-			else if ((val > m_dPeakPoint) && (val < (m_dPeakPoint - m_dRightOffset))) {
-				float grad = 1.0f / -m_dRightOffset;
-				return grad * (val - m_dPeakPoint) + 1.0f;
-			}
-			// out of range of this FLV, return zero
-			else {
-				return 0.0f;
+			} else if ((val > m_dPeakPoint) && (val < (m_dPeakPoint - m_dRightOffset))) {
+				float grad = 1 / -m_dRightOffset;
+				return grad * (val - m_dPeakPoint) + 1;
+			} else {
+				return 0;
 			}
 		}
 
-		public float RepresentativeValue() {
-			return m_dPeakPoint;
-		}
+		public float RepresentativeValue() { return m_dPeakPoint; }
 
-		// the values that define the shape of this FLV
 		float m_dPeakPoint;
 		float m_dLeftOffset;
 		float m_dRightOffset;
 	}
 
 	public class FuzzySet_Singleton : FuzzySet {
-		public FuzzySet_Singleton(float mid,
-		                          float lft,
-		                          float rgt) : base(mid) {
-			m_dMidPoint = mid;
-			m_dLeftPoint = lft;
+		public FuzzySet_Singleton(float mid, float lft, float rgt) : base(mid) {
+			m_dMidPoint   = mid;
+			m_dLeftPoint  = lft;
 			m_dRightPoint = rgt;
 		}
 
 		override public float CalculateDOM(float val) {
 			if ((val >= m_dMidPoint - m_dLeftPoint) &&
 			    (val <= m_dMidPoint + m_dRightPoint)) {
-				return 1.0f;
+				return 1;
 			} else {
-				return 0.0f;
+				return 0;
 			}
 		}
 
