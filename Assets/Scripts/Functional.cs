375948def838724beb1e090151dcae3440a140c3
SethSR <srenshaw1@gmail.com>
2015-09-28 01:23:13 -0500

    Added a functional-style ForEach method

---
+++ Assets/Scripts/Functional.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Functional {
+	static public class FuncForEach {
+		static public void ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+			foreach (var item in source) {
+				action(item);
+			}
+		}
+	}
+}
