451c83f79d86637e7d6f24e3743b8b40df5cc966
SethSR <srenshaw1@gmail.com>
2015-09-28 01:22:51 -0500

    removed Lerping to the target

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -6,10 +6,8 @@ public class Follow : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		var pos = (Vector2)transform.position;
-		var tgt = (Vector2)target.position;
-		var dist = (tgt - pos).magnitude;
-		var pos2 = Vector2.Lerp(pos, tgt, 1 / dist);
-		transform.position = new Vector3(pos2.x, pos2.y, transform.position.z);
+		transform.position = new Vector3(target.position.x,
+		                                 target.position.y,
+		                                 transform.position.z);
 	}
 }
